927aceeb2988ba9e955bb5a738de0d3451ce7d27
ac-he <98062893+ac-he@users.noreply.github.com>
2024-08-29 17:57:45 -0500

    Add Fantasy Flags to Presets

--- presets/preset_flag_collections.py
+++ presets/preset_flag_collections.py
@@ -1,6 +1,31 @@
 pre_flag_collection = {
     "fantasy": [
         {
+            "shape": "heater",
+            "field": {
+                "party": "per saltire",
+                "chief": {
+                    "tincture": "a",
+                    "charge": "octofoil",
+                    "charge-tincture": "r"
+                },
+                "base": {
+                    "tincture": "a",
+                    "charge": "unicorn",
+                },
+                "dexter": {
+                    "tincture": "o",
+                    "charge": "octofoil",
+                    "charge-tincture": "r"
+                },
+                "sinister": {
+                    "tincture": "o",
+                    "charge": "octofoil",
+                    "charge-tincture": "r"
+                },
+            }
+        },
+        {
             "shape": "banner",
             "field": {
                 "party": "per fess",
@@ -40,12 +65,136 @@ pre_flag_collection = {
             }
         },
         {
+            "shape": "rect",
+            "field": {
+                "party": "per chevron",
+                "chief": {
+                    "tincture": "o",
+                    "charge": "rose",
+                    "quantity": 1,
+                    "charge-tincture": "r",
+                },
+                "base": {
+                    "tincture": "r",
+                    "charge": "harp",
+                    "quantity": 2,
+                }
+            }
+        },
+        {
             "shape": "pennant",
             "field": {
                 "party": "none",
                 "field": {
                     "tincture": "a",
                     "charge": "yale"
+                },
+            }
+        },
+        {
+            "shape": "heater",
+            "field": {
+                "party": "per saltire",
+                "base": {
+                    "tincture": "p",
+                    "charge": "billet",
+                    "quantity": 3,
+                    "charge-tincture": "o",
+                },
+                "chief": {
+                    "tincture": "p",
+                    "charge": "billet",
+                    "quantity": 3,
+                    "charge-tincture": "o",
+                },
+                "dexter": {
+                    "tincture": "o",
+                    "charge": "billet",
+                    "quantity": 3,
+                    "charge-tincture": "p",
+                },
+                "sinister": {
+                    "tincture": "o",
+                    "charge": "billet",
+                    "quantity": 3,
+                    "charge-tincture": "p",
+                },
+            }
+        },
+        {
+            "shape": "banner",
+            "field": {
+                "party": "none",
+                "field": {
+                    "tincture": "r",
+                    "quantity": 3,
+                    "charge": "bee"
+                },
+            }
+        },
+        {
+            "shape": "rect",
+            "field": {
+                "party": "per pale",
+                "dexter": {
+                    "tincture": "r",
+                    "charge-tincture": ["p", "r", "p", "r", "p", "r", "p", "r"],
+                    "charge": "quarterly_of_eight"
+                },
+                "sinister": {
+                    "tincture": "r",
+                    "charge-tincture": ["p", "r", "p", "r", "p", "r", "p", "r"],
+                    "charge": "quarterly_of_eight"
+                }
+            }
+        },
+        {
+            "shape": "pennant",
+            "field": {
+                "party": "per cross",
+                "dexter-chief": {
+                    "tincture": "a",
+                    "charge": "sun"
+                },
+                "dexter-base": {
+                    "tincture": "o"
+                },
+                "sinister-chief": {
+                    "tincture": "o"
+                },
+                "sinister-base": {
+                    "tincture": "o"
+                },
+            }
+        },
+        {
+            "shape": "heater",
+            "field": {
+                "party": "per pall",
+                "dexter": {
+                    "tincture": "p",
+                    "charge": "castle",
+                    "quantity": 2
+                },
+                "chief": {
+                    "tincture": "r",
+                    "charge": "gorge",
+                    "charge-tincture": "a"
+                },
+                "sinister": {
+                    "tincture": "p",
+                    "charge": "castle",
+                    "quantity": 2
+                },
+            }
+        },
+        {
+            "shape": "pennant",
+            "field": {
+                "party": "none",
+                "field": {
+                    "tincture": "r",
+                    "charge": "unicorn"
                 },
             }
         },
